4f27b93e825b5274808a5aa24f802bdf336b61e3
Fernando de Oliveira <ffernandodeoliveira@gmail.com>
2022-09-10 09:46:43 -0300

    Funcionalidades da aplicação - Interromper ciclo

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,6 +2,7 @@ import styled from 'styled-components'
 
 export const HomeContainer = styled.main`
   flex: 1;
+
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -41,6 +42,7 @@ const BaseInput = styled.input`
     box-shadow: none;
     border-color: ${(props) => props.theme['green-500']};
   }
+
   &::placeholder {
     color: ${(props) => props.theme['gray-500']};
   }
@@ -63,8 +65,10 @@ export const CountdownContainer = styled.div`
   font-size: 10rem;
   line-height: 8rem;
   color: ${(props) => props.theme['gray-100']};
+
   display: flex;
   gap: 1rem;
+
   span {
     background: ${(props) => props.theme['gray-700']};
     padding: 2rem 1rem;
@@ -74,30 +78,48 @@ export const CountdownContainer = styled.div`
 export const Separator = styled.div`
   padding: 2rem 0;
   color: ${(props) => props.theme['green-500']};
+
   width: 4rem;
   overflow: hidden;
   display: flex;
   justify-content: center;
 `
 
-export const StartCountdownButton = styled.button`
+export const BaseCountdownButton = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
   border-radius: 8px;
+
   display: flex;
   align-items: center;
   justify-content: center;
+  color: ${(props) => props.theme['gray-100']};
+
   gap: 0.5rem;
   font-weight: bold;
+
   cursor: pointer;
-  background: ${(props) => props.theme['green-500']};
-  color: ${(props) => props.theme['gray-100']};
+
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
+
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButton)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
   }
 `
